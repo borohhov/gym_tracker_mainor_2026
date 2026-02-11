0b53e704a4fbd68fb7bf5aea13c9f31cbcc85de0
borohhov <dmitri@ausalt.com>
2026-02-11 10:03:56 +0200

    Building the form

--- lib/views/exercise_log_entry_form.dart
+++ lib/views/exercise_log_entry_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gym_tracker/models/default_exercises.dart';
+import 'package:gym_tracker/models/exercise_log.dart';
 
 import '../models/exercise.dart';
 
@@ -12,13 +13,14 @@ class ExerciseLogEntryForm extends StatefulWidget {
 }
 
 class _ExerciseLogEntryFormState extends State<ExerciseLogEntryForm> {
-  Exercise? _selected; // adjust type to whatever your list contains
+  late ExerciseLog exerciseLog;
+  Exercise? _selected;
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
-        mainAxisSize: MainAxisSize.min, // ✅ prevents dialog overflow
+        mainAxisSize: MainAxisSize.min,
         children: [
           DropdownButtonFormField<Exercise>(
             value: _selected,
@@ -26,18 +28,54 @@ class _ExerciseLogEntryFormState extends State<ExerciseLogEntryForm> {
             items: defaultExercises
                 .map(
                   (ex) => DropdownMenuItem<Exercise>(
-                value: ex,              // ✅ IMPORTANT
-                child: Text(ex.name),
-              ),
-            )
+                    value: ex,
+                    child: Text(ex.name),
+                  ),
+                )
                 .toList(),
             onChanged: (value) {
-              setState(() => _selected = value);
+              setState(() {
+                _selected = value;
+                exerciseLog.exercise = _selected!;
+              });
             },
+          ),
+          Text('First set'),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: 'Reps'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        int.tryParse(value) != null) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: 'Weight'),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        int.tryParse(value) != null) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
